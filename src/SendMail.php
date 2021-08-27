29fe217a5c1490c0dea5d3d198c9c4d35bdc0745
Sudip Dahal <47928695+sid509@users.noreply.github.com>
2021-08-27 17:33:31 +0530

    Update SendMail.php

--- src/SendMail.php
+++ src/SendMail.php
@@ -5,6 +5,9 @@ class SendMail
     public static function generateQr($number,$message)
     {
         $urlQr ='https://api.qrserver.com/v1/create-qr-code/?data=https://wa.me/'.$number.'/?text='.$message;
-        return $urlQr;
+        $base_url = 'http://tinyurl.com/api-create.php?url=';
+        $url = $base_url .''. $urlQr;
+        $turl=Http::get($url);
+        return $turl;
     }
 }
